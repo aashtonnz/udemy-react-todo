61dc95f5b958c92430364350c029c19dc3bf9d98
Andrew Ashton <aashtonnz@gmail.com>
2019-04-22 12:57:50 +1200

    Add array destructuring

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,17 +1,17 @@
 import React, { useState } from 'react';
 
 const todo = (props) => {
-  const inputState = useState('');
+  const [todoState, setTodoState] = useState('');
 
   const inputChangeHandler = (event) => {
-    inputState[1](event.target.value);
+    setTodoState(event.target.value);
   };
 
   return <React.Fragment>
     <input
       type="text"
       placeholder="Todo"
-      value={inputState[0]}
+      value={todoState}
       onChange={inputChangeHandler} />
     <button>Add</button>
     <ul>
